992c277830a86b644ad1dc859bad1e4efcf18c7d
Ariva2 <ariva080503@gmail.com>
2023-11-15 10:08:48 +0700

    Initial commit

---
+++ src/main/java/Aplikasi_Perhitungan_Jumlah_Hari.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ariva
+ */
+public class Aplikasi_Perhitungan_Jumlah_Hari {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
